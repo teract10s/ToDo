6f8c60e03051a60471a9eb1b03e1ebc18fcfd03a
Taras Leonovets <Taras.Leonovets@nangasystems.com>
2024-10-23 23:08:57 +0300

    added possibility to connect to server from local network

--- src/main/java/pet/proj/todo/config/WebConfig.java
+++ src/main/java/pet/proj/todo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://192.168.0.101:3000")
+                        .allowedOriginPatterns("http://*:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
